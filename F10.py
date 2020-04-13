e7099583bc720551637c3b09ea9b381ed21ff078
stellaribli <51784762+stellaribli@users.noreply.github.com>
2020-04-14 02:14:19 +0700

    F10.py

---
+++ F10.py
@@ -0,0 +1,16 @@
+#FUNGSI F10, FUNGSI MELIHAT KRITIK SARAN
+import csv
+
+def lihat_laporan(a):
+    print("Kritik dan saran:")
+    f = open('kritiksaran.csv', 'r')
+    with f:
+        reader = csv.reader(f,delimiter=';')
+        next(reader)        #agar row pertama tidak termasuk di output
+        for row in reader:
+            print(row[2]+" | " + row[1] +" | " +row[0]+" | " + row[3]) #output kritik
+    f.close()
+    return
+
+lihat_laporan("")
+
