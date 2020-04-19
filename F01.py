272fc5edc6044e6f793c8ec9eba4bfe34c30b701
stellaribli <51784762+stellaribli@users.noreply.github.com>
2020-04-19 18:07:45 +0700

    Update F01.py

--- F01.py
+++ F01.py
@@ -3,14 +3,13 @@ import csv
 
 #Fungsi untuk membaca csv dan mengubah bentuk menjadi array
 def imp(a):
-    csvfile = open(a, "r")
-    with csvfile:
+	csvfile = open(a, "r")
+	with csvfile:
         # FUNGSI MEMBACA FILE BENTUK CSV
-        reader = csv.reader(csvfile, delimiter=';')
-        listreader = list(reader)
-    return listreader
-
-#INPUT UNTUK MEMASUKKAN NAMA FILE
+		reader = csv.reader(csvfile, delimiter=',')
+		listreader = [i for i in reader]
+	return listreader
+	#INPUT UNTUK MEMASUKKAN NAMA FILE
 A=input("Masukkan nama File User: ")
 user=imp(A)
 B=input("Masukkan nama File Daftar Wahana: ")
@@ -26,3 +25,20 @@ refund=imp(F)
 G=input("Masukkan nama File Kritik dan Saran: ")
 kritiksaran=imp(G)
 print("File perusahaan Willy Wangky's Chocolate Factory telah di-load.")
+
+	#Fungsi untuk digunakan dalam program utama
+def pilihan(A):
+	if A == "User.csv":
+		return user
+	elif A == "wahana.csv":
+		return wahana
+	elif A == "pembelian.csv":
+		return pembelian
+	elif A == "penggunaan.csv":
+		return penggunaan
+	elif A == "tiket.csv":
+		return tiket
+	elif A == "refund.csv":
+		return refund
+	elif A == "kritiksaran.csv":
+		return kritiksaran
