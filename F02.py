dcf38a7ed55af25d916f086a795bfe0e799a25a3
Uyamikun <63537058+Uyamikun@users.noreply.github.com>
2020-04-19 19:35:19 +0700

    Update F02.py

--- F02.py
+++ F02.py
@@ -1,26 +1,21 @@
-import csv
+import F01
 
-def save(namafile):
-	with open(namafile, 'a', newline='') as csvfile:
-		filesave = csv.writer(csvfile,delimiter=",")
-		if namafile == "user.csv":
-			filesave.writerow([Nama,Tanggal_Lahir,Tinggi_Badan,Username,Password,Role,Saldo])
-			return filesave
-		elif namafile == 'wahana.csv':
-			filesave.writerow([ID_Wahana,Nama_Wahana,Harga_Tiket,Batasan_Umur,Batasan_Tinggi])
-			return filesave
-		elif namafile == 'pembelian.csv':
-			filesave.writerow([Username,Tanggal_Pembelian,ID_Wahana,Jumlah_Tiket])
-			return filesave
-		elif namafile == 'penggunaan.csv':
-			filesave.writerow([Username,Tanggal_Penggunaan,ID_Wahana,Jumlah_Tiket])
-			return filesave
-		elif namafile == 'tiket.csv':
-			filesave.writerow([Username,ID_Wahana,Jumlah_Tiket])
-			return filesave
-		elif namafile == 'refund.csv':
-			filesave.writerow([Username,Tanggal_Refund,ID_Wahana,Jumlah_Tiket])
-			return filesave
-		else:
-			filesave.write([Username,Tanggal_Kritik,ID_Wahana,Isi_Kritik])
-			return filesave
+def login(username, password):
+	user = F01.pilihan("user.csv")
+	check = False
+	while check == False :
+		i = 0
+		while i <=9: #jumlah data di file csv
+			if (user[i][3] == username and user[i][4] == password):
+				check = True
+				break
+			i = i + 1
+		if check == False: # masih salah kalau ga ada usernamenya, index out of range karena list dari F01nya error
+			print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi")
+			username = input("Masukkan username: ")
+			password = input("Masukkan password: ")			
+	return ("Selamat bersenang - senang, " + username)
+
+username = input("Masukkan username: ")
+password = input("Masukkan password: ")
+print(login(username,password))
