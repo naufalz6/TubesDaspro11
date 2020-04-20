2a72a3dd2bfdc4138175e874e72e7bc244215e92
zharfanf <57822301+zharfanf@users.noreply.github.com>
2020-04-20 22:32:09 +0700

    Add files via upload

---
+++ F14.py
@@ -0,0 +1,17 @@
+import F01
+
+def riwayat(ID):
+	penggunaan = F01.pilihan("penggunaan.csv")
+	hitung = 0
+	print("Riwayat:")
+	for i in range(11):
+		if penggunaan[i][2] == ID :
+			hitung = hitung + 1
+			print(penggunaan[i][1] + " | " + penggunaan[i][0] + " | " + penggunaan[i][3])
+	if hitung == 0:
+		print("Maaf, belum ada riwayat untuk wahana ini.")
+	return penggunaan
+
+x  = input("Masukan ID wahana : ")
+
+riwayat(x)
