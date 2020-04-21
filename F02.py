da0fde2f88058933681aba68d9c90622d3c0a42d
Uyamikun <63537058+Uyamikun@users.noreply.github.com>
2020-04-21 10:29:15 +0700

    Add files via upload

--- F02.py
+++ F02.py
@@ -5,12 +5,12 @@ def login(username, password):
 	check = False
 	while check == False :
 		i = 0
-		while i <=9: #jumlah data di file csv
+		while i <=9:
 			if (user[i][3] == username and user[i][4] == password):
 				check = True
 				break
 			i = i + 1
-		if check == False: # masih salah kalau ga ada usernamenya, index out of range karena list dari F01nya error
+		if check == False:
 			print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi")
 			username = input("Masukkan username: ")
 			password = input("Masukkan password: ")			
