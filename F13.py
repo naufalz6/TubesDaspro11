3c52e02eaa0450d8f060762b3abe55ad0e732915
renanthera216 <63538733+renanthera216@users.noreply.github.com>
2020-04-22 11:45:19 +0700

    Add files via upload

---
+++ F13.py
@@ -0,0 +1,22 @@
+#F13, FUNGSI TOP UP SALDO
+
+import csv
+
+def topup():
+
+    uname=input("Masukkan username: ")
+    up=int(input("Masukkan saldo yang di-top up: ")
+    
+    f=open("user.csv", "w")
+    reader=csv.reader(f, delimiter=";")
+
+    with f:
+           next(reader)
+           for row in reader:
+               if (row[3]==uname):
+                   row[6]=str(int(row[6])+up)
+                   print("Top up berhasil. Saldo "+uname+" bertambah menjadi "+row[6])
+    f.close()
+    return
+
+    
