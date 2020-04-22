7942eccd889d930fefe9c0fdfbe42ddbbe683cf1
Faishal Zharfan <zharfanf@gmail.com>
2020-04-22 21:13:56 +0700

    check

--- F02.py
+++ F02.py
@@ -3,13 +3,11 @@ import F01
 def login(username, password):
 	user = F01.pilihan("user.csv")
 	check = False
-	while check == False :
-		i = 0
-		while i <=9:
+	while not(check) :
+		for i in range(11):
 			if (user[i][3] == username and user[i][4] == password):
 				check = True
 				break
-			i = i + 1
 		if check == False:
 			print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi")
 			username = input("Masukkan username: ")
