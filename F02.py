4f0962845b35c451cd48153b0be4a8007e85837a
Uyamikun <63537058+Uyamikun@users.noreply.github.com>
2020-04-23 01:01:01 +0700

    Update F02.py

--- F02.py
+++ F02.py
@@ -1,10 +1,11 @@
-import F01
+import F01 as load
+import F03 as sv
 
 def login(username, password):
-	user = F01.pilihan("user.csv")
+	user = load.pilihan("user.csv")
 	check = False
 	while not(check) :
-		for i in range(11):
+		for i in range(sv.countdata("user.csv")):
 			if (user[i][3] == username and user[i][4] == password):
 				check = True
 				break
@@ -17,3 +18,5 @@ def login(username, password):
 username = input("Masukkan username: ")
 password = input("Masukkan password: ")
 print(login(username,password))
+
+#Blm di cek ulang per 23/4/2020 jam 1:00 AM
