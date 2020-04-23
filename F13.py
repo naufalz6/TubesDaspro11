6e01c16c9fc6a9e4465366b8e930a10c71fdb8b7
renanthera216 <63538733+renanthera216@users.noreply.github.com>
2020-04-24 00:44:30 +0700

    Update F13.py

--- F13.py
+++ F13.py
@@ -1,25 +1,23 @@
 #F13, FUNGSI TOP UP SALDO
 
-import csv
+import F01
 
 def topup():
 
     uname=input("Masukkan username: ")
     up=int(input("Masukkan saldo yang di-top up: ")
     
-    f=open("user.csv", "w")
-    reader=csv.reader(f, delimiter=";")
-
-    with f:
-           next(reader)
-           for row in reader:
-               if (row[3]==uname):
-                   row[6]=str(int(row[6])+up)
-                   print("Top up berhasil. Saldo "+uname+" bertambah menjadi "+row[6])
-    f.close()
+    user = F01.pilihan("user.csv")
+    i = 0
+    for i in range (11):
+           if(user[i][3]==uname):
+           user[i][6]=str(int(user[i][6])+up)
+           print("Top up berhasil. Saldo "+uname+" bertambah menjadi "+user[i][6])
+    
     return
 
 # Kasih masukan ya
 # programnya gak usah import csv lagi, jadi kita di programnya mainin array aja
 # ubah lagi ya Ren :), semangatttt!!!!!
     
+# 24/4/2020 udah diubah sal, coba diperiksa lagi, thankss:))
