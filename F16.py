f693cfb5727cadb6d4316aa779b9b0a58da1e2a1
stellaribli <51784762+stellaribli@users.noreply.github.com>
2020-04-23 23:24:35 +0700

    Update F16.py

--- F16.py
+++ F16.py
@@ -1,11 +1,11 @@
 #F06, Fungsi Exit
 def exit():
-    exit=input("Apakah anda mau melakukan penyimpanan file yang sudah dilakukan (Y/N) ? ")
+    pilihan=input("Apakah anda mau melakukan penyimpanan file yang sudah dilakukan (Y/N) ? ")
 
-    while (exit!="Y" and exit!="N"):
-        exit = input("Apakah anda mau melakukan penyimpanan file yang sudah dilakukan (Y/N) ? ")
+    while (pilihan!="Y" and pilihan!="N"):
+        pilih = input("Apakah anda mau melakukan penyimpanan file yang sudah dilakukan (Y/N) ? ")
 
-    if (exit == "Y"):
+    if (pilihan == "Y"):
         save()
 
     return
