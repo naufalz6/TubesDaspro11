8fa7cb398629f6bdd66d79b223d0f1c766405532
Uyamikun <63537058+Uyamikun@users.noreply.github.com>
2020-04-24 13:45:35 +0700

    Update F07.py

--- F07.py
+++ F07.py
@@ -1,33 +1,27 @@
-
-
 import F01
 import F03 
 
-def beli_tiket(ID_Wahana,Tanggal,Jumlah):
+def beli_tiket(username,Tanggal,Jumlah):
 	user = F01.pilihan("user.csv")
 	wahana = F01.pilihan("wahana.csv")
-	nEff = F03.countdata("wahana.csv")
-	for i in range(nEff):
+	for i in range(11):
+		if user[i][0] == username:
+			p = i
+			break
+	for i in range(11):
 		if wahana[i][0] == ID_Wahana:
 			ind = i
 			break
-	if int(user[][2]) < int(wahana[ind][4]):
+	arrtglluser = [user[p][1]]
+	arrtglwhn = [Tanggal]
+	if int(user[p][2]) < int(wahana[ind][4]):
 		return("Anda tidak memenuhi persyaratan untuk memainkan wahana ini. Silakan menggunakan wahana lain yang tersedia")
 	else:
-		if :#cek umur ga memenuhi
+		if(int(arrtglwhn[0][:6]) - int(arrtglluser[0][:6]))*12 < (wahana[ind][3])*12 :#cek umur ga memenuhi
 			return("Anda tidak memenuhi persyaratan untuk memainkan wahana ini. Silakan menggunakan wahana lain yang tersedia")
 		else:
-			if (int(user[][6]) - (int(wahana[ind][2])*Jumlah))<0 :
+			if (int(user[p][6]) - (int(wahana[ind][2])*Jumlah)) < 0 :
 				return("Saldo Anda tidak cukup. Silakan mengisi saldo Anda")
 			else:
-				save # save nama,IDwahana,tanggal beli, jumlah tiket pke fungsi F03
+				save # save nama,IDwahana,tanggal beli, jumlah tiket pke fungsi F03 ke file pembelian tiket
 				return("Selamat bersenang-senang di" + wahana[ind][1])
-
-
-
-ID_Wahana = input("Masukkan ID wahana: ")
-Tanggal = input("Masukkan tanggal hari ini: ")
-Jumlah = input("Jumlah tiket yan dibeli")
-print(beli_tiket())
-
-#BELUM SELESAI HEHEHE TINGGAL UMUR SAMA SAVENYA
