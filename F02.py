69d99e90cb43d39218edcfb0be51c73f0ad41f93
Uyamikun <63537058+Uyamikun@users.noreply.github.com>
2020-04-24 10:38:22 +0700

    Update F02.py

--- F02.py
+++ F02.py
@@ -1,23 +1,17 @@
 import F01 as load
-import F03 as sv
 
 def login(username, password):
 	user = load.pilihan("user.csv")
 	check = False
 	while not(check) :
-		for i in range(sv.countdata("user.csv")):
+		for i in range(load.countdata("user.csv")):
 			if (user[i][3] == username and user[i][4] == password):
 				check = True
 				break
 		if check == False:
-			print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi")
-			username = input("Masukkan username: ")
-			password = input("Masukkan password: ")			
+			return("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi")		
 	return ("Selamat bersenang - senang, " + username)
 
-username = input("Masukkan username: ")
-password = input("Masukkan password: ")
-print(login(username,password))
 
 #Blm di cek ulang per 23/4/2020 jam 1:00 AM
 #tolong di cek karena ada yang berubah per 23/4/2020 jam 1:00 AM
