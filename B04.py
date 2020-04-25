ff77736ef99d348b3e834680cc91e5e9dbbac087
renanthera216 <63538733+renanthera216@users.noreply.github.com>
2020-04-25 16:50:48 +0700

    tolong dicek hehe

--- B04.py
+++ B04.py
@@ -1,7 +1,6 @@
 #B04, Fungsi Laporan Kehilangan Tiket
 
-import F01 as load
-import csv
+import F01
 
 def tiket_hilang():
 
@@ -10,21 +9,19 @@ def tiket_hilang():
     idw=input("ID Wahana: ")
     n=int(input("Jumlah tiket yang dihilangkan: ")
 
-    tiket=load.pilihan("tiket.csv")
+    tiket=F01.pilihan("tiket.csv")
     i=0
     for i in range (11):
           if (tiket[i][0]==uname) and (tiket[i][1]==idw):
               tiket[i][3]=str(int(tiket[i][2])-n)
 
-    with open("kehilangan.csv", "w") as f:
-          reader=csv.reader(f, delimiter=";")
-          i=0
-          while i<11 :
-              i+=1
-              kehilangan[i][0]=kehilangan[i][0]+uname
-              kehilangan[i][1]=kehilangan[i][1]+date
-              kehilangan[i][2]=kehilangan[i][2]+idw
-              kehilangan[i][3]=kehilangan[i][3]+n
-              print("Laporan kehilangan tiket Anda telah direkam.")
-    f.close()
+    kehilangan=F01.imp("kehilangan.csv")
+    i=0
+    while i<11 :
+          i+=1
+          kehilangan[i][0]=kehilangan[i][0]+uname
+          kehilangan[i][1]=kehilangan[i][1]+date
+          kehilangan[i][2]=kehilangan[i][2]+idw
+          kehilangan[i][3]=kehilangan[i][3]+n
+    print("Laporan kehilangan tiket Anda telah direkam.")
     return
